d2cc74e23fee5f913f1b418d947bc1f7a419859c
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2024-10-16 17:20:54 -0300

    Adicionar arquivos de projeto.

---
+++ SysTINSApp/FrmPrincipal.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SysTINSApp
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem = new ToolStripMenuItem();
+            buscarToolStripMenuItem = new ToolStripMenuItem();
+            produtosToolStripMenuItem = new ToolStripMenuItem();
+            novoInserirToolStripMenuItem = new ToolStripMenuItem();
+            buscarToolStripMenuItem1 = new ToolStripMenuItem();
+            usuáriosToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem1 = new ToolStripMenuItem();
+            consultarToolStripMenuItem = new ToolStripMenuItem();
+            niveisToolStripMenuItem = new ToolStripMenuItem();
+            movimentoToolStripMenuItem = new ToolStripMenuItem();
+            pedidoToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem2 = new ToolStripMenuItem();
+            consultarToolStripMenuItem1 = new ToolStripMenuItem();
+            listarToolStripMenuItem = new ToolStripMenuItem();
+            orçamentoToolStripMenuItem = new ToolStripMenuItem();
+            trocasEDevoluçõesToolStripMenuItem = new ToolStripMenuItem();
+            materiaisToolStripMenuItem = new ToolStripMenuItem();
+            caixaToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            consultarToolStripMenuItem2 = new ToolStripMenuItem();
+            encerrarToolStripMenuItem = new ToolStripMenuItem();
+            listarToolStripMenuItem1 = new ToolStripMenuItem();
+            vendaToolStripMenuItem = new ToolStripMenuItem();
+            manutençãoToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastrosToolStripMenuItem, movimentoToolStripMenuItem, materiaisToolStripMenuItem, manutençãoToolStripMenuItem, sairToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clientesToolStripMenuItem, produtosToolStripMenuItem, usuáriosToolStripMenuItem });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(71, 20);
+            cadastrosToolStripMenuItem.Text = "&Cadastros";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem, buscarToolStripMenuItem });
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(180, 22);
+            clientesToolStripMenuItem.Text = "&Clientes";
+            // 
+            // novoToolStripMenuItem
+            // 
+            novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            novoToolStripMenuItem.Size = new Size(180, 22);
+            novoToolStripMenuItem.Text = "&Novo";
+            // 
+            // buscarToolStripMenuItem
+            // 
+            buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            buscarToolStripMenuItem.Size = new Size(180, 22);
+            buscarToolStripMenuItem.Text = "&Buscar";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            produtosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoInserirToolStripMenuItem, buscarToolStripMenuItem1 });
+            produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            produtosToolStripMenuItem.Size = new Size(180, 22);
+            produtosToolStripMenuItem.Text = "&Produtos";
+            // 
+            // novoInserirToolStripMenuItem
+            // 
+            novoInserirToolStripMenuItem.Name = "novoInserirToolStripMenuItem";
+            novoInserirToolStripMenuItem.Size = new Size(180, 22);
+            novoInserirToolStripMenuItem.Text = "&Novo (Inserir)";
+            // 
+            // buscarToolStripMenuItem1
+            // 
+            buscarToolStripMenuItem1.Name = "buscarToolStripMenuItem1";
+            buscarToolStripMenuItem1.Size = new Size(180, 22);
+            buscarToolStripMenuItem1.Text = "&Buscar";
+            // 
+            // usuáriosToolStripMenuItem
+            // 
+            usuáriosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem1, consultarToolStripMenuItem, niveisToolStripMenuItem });
+            usuáriosToolStripMenuItem.Name = "usuáriosToolStripMenuItem";
+            usuáriosToolStripMenuItem.Size = new Size(180, 22);
+            usuáriosToolStripMenuItem.Text = "&Usuários";
+            // 
+            // novoToolStripMenuItem1
+            // 
+            novoToolStripMenuItem1.Name = "novoToolStripMenuItem1";
+            novoToolStripMenuItem1.Size = new Size(180, 22);
+            novoToolStripMenuItem1.Text = "&Novo";
+            novoToolStripMenuItem1.Click += novoToolStripMenuItem1_Click;
+            // 
+            // consultarToolStripMenuItem
+            // 
+            consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
+            consultarToolStripMenuItem.Size = new Size(180, 22);
+            consultarToolStripMenuItem.Text = "&Consultar";
+            // 
+            // niveisToolStripMenuItem
+            // 
+            niveisToolStripMenuItem.Name = "niveisToolStripMenuItem";
+            niveisToolStripMenuItem.Size = new Size(180, 22);
+            niveisToolStripMenuItem.Text = "&Niveis";
+            // 
+            // movimentoToolStripMenuItem
+            // 
+            movimentoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { pedidoToolStripMenuItem, orçamentoToolStripMenuItem, trocasEDevoluçõesToolStripMenuItem });
+            movimentoToolStripMenuItem.Name = "movimentoToolStripMenuItem";
+            movimentoToolStripMenuItem.Size = new Size(81, 20);
+            movimentoToolStripMenuItem.Text = "&Movimento";
+            // 
+            // pedidoToolStripMenuItem
+            // 
+            pedidoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem2, consultarToolStripMenuItem1, listarToolStripMenuItem });
+            pedidoToolStripMenuItem.Name = "pedidoToolStripMenuItem";
+            pedidoToolStripMenuItem.Size = new Size(180, 22);
+            pedidoToolStripMenuItem.Text = "&Pedido";
+            // 
+            // novoToolStripMenuItem2
+            // 
+            novoToolStripMenuItem2.Name = "novoToolStripMenuItem2";
+            novoToolStripMenuItem2.Size = new Size(180, 22);
+            novoToolStripMenuItem2.Text = "&Novo";
+            // 
+            // consultarToolStripMenuItem1
+            // 
+            consultarToolStripMenuItem1.Name = "consultarToolStripMenuItem1";
+            consultarToolStripMenuItem1.Size = new Size(180, 22);
+            consultarToolStripMenuItem1.Text = "&Consultar";
+            // 
+            // listarToolStripMenuItem
+            // 
+            listarToolStripMenuItem.Name = "listarToolStripMenuItem";
+            listarToolStripMenuItem.Size = new Size(180, 22);
+            listarToolStripMenuItem.Text = "&Listar";
+            // 
+            // orçamentoToolStripMenuItem
+            // 
+            orçamentoToolStripMenuItem.Name = "orçamentoToolStripMenuItem";
+            orçamentoToolStripMenuItem.Size = new Size(180, 22);
+            orçamentoToolStripMenuItem.Text = "&Orçamento";
+            // 
+            // trocasEDevoluçõesToolStripMenuItem
+            // 
+            trocasEDevoluçõesToolStripMenuItem.Name = "trocasEDevoluçõesToolStripMenuItem";
+            trocasEDevoluçõesToolStripMenuItem.Size = new Size(180, 22);
+            trocasEDevoluçõesToolStripMenuItem.Text = "&Trocas e Devoluções";
+            // 
+            // materiaisToolStripMenuItem
+            // 
+            materiaisToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { caixaToolStripMenuItem, vendaToolStripMenuItem });
+            materiaisToolStripMenuItem.Name = "materiaisToolStripMenuItem";
+            materiaisToolStripMenuItem.Size = new Size(70, 20);
+            materiaisToolStripMenuItem.Text = "&Operação";
+            // 
+            // caixaToolStripMenuItem
+            // 
+            caixaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { abrirToolStripMenuItem, consultarToolStripMenuItem2, encerrarToolStripMenuItem, listarToolStripMenuItem1 });
+            caixaToolStripMenuItem.Name = "caixaToolStripMenuItem";
+            caixaToolStripMenuItem.Size = new Size(180, 22);
+            caixaToolStripMenuItem.Text = "&Caixa";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(180, 22);
+            abrirToolStripMenuItem.Text = "&Abrir";
+            // 
+            // consultarToolStripMenuItem2
+            // 
+            consultarToolStripMenuItem2.Name = "consultarToolStripMenuItem2";
+            consultarToolStripMenuItem2.Size = new Size(180, 22);
+            consultarToolStripMenuItem2.Text = "&Consultar";
+            // 
+            // encerrarToolStripMenuItem
+            // 
+            encerrarToolStripMenuItem.Name = "encerrarToolStripMenuItem";
+            encerrarToolStripMenuItem.Size = new Size(180, 22);
+            encerrarToolStripMenuItem.Text = "&Encerrar";
+            // 
+            // listarToolStripMenuItem1
+            // 
+            listarToolStripMenuItem1.Name = "listarToolStripMenuItem1";
+            listarToolStripMenuItem1.Size = new Size(180, 22);
+            listarToolStripMenuItem1.Text = "&Listar";
+            // 
+            // vendaToolStripMenuItem
+            // 
+            vendaToolStripMenuItem.Name = "vendaToolStripMenuItem";
+            vendaToolStripMenuItem.Size = new Size(180, 22);
+            vendaToolStripMenuItem.Text = "&Venda";
+            // 
+            // manutençãoToolStripMenuItem
+            // 
+            manutençãoToolStripMenuItem.Name = "manutençãoToolStripMenuItem";
+            manutençãoToolStripMenuItem.Size = new Size(86, 20);
+            manutençãoToolStripMenuItem.Text = "M&anutenção";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(87, 20);
+            sairToolStripMenuItem.Text = "&Sair (Logout)";
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "FrmPrincipal";
+            Text = "SysTINS - Syspeças TI97 - ver 0.0.1";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem;
+        private ToolStripMenuItem buscarToolStripMenuItem;
+        private ToolStripMenuItem produtosToolStripMenuItem;
+        private ToolStripMenuItem novoInserirToolStripMenuItem;
+        private ToolStripMenuItem buscarToolStripMenuItem1;
+        private ToolStripMenuItem usuáriosToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem1;
+        private ToolStripMenuItem consultarToolStripMenuItem;
+        private ToolStripMenuItem niveisToolStripMenuItem;
+        private ToolStripMenuItem movimentoToolStripMenuItem;
+        private ToolStripMenuItem pedidoToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem2;
+        private ToolStripMenuItem consultarToolStripMenuItem1;
+        private ToolStripMenuItem listarToolStripMenuItem;
+        private ToolStripMenuItem orçamentoToolStripMenuItem;
+        private ToolStripMenuItem trocasEDevoluçõesToolStripMenuItem;
+        private ToolStripMenuItem materiaisToolStripMenuItem;
+        private ToolStripMenuItem caixaToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem consultarToolStripMenuItem2;
+        private ToolStripMenuItem encerrarToolStripMenuItem;
+        private ToolStripMenuItem listarToolStripMenuItem1;
+        private ToolStripMenuItem vendaToolStripMenuItem;
+        private ToolStripMenuItem manutençãoToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ SysTINSClass/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace SysTINSClass
+{
+    public class Class1
+    {
+
+        public int Id { get; set; }
+
+        // XML Docs
+        /// <summary>
+        /// Calcula o valor das parcelas pela divisão do montante pelo número de parcelas
+        /// respeitando as RNs 25 e 26.
+        /// </summary>
+        /// <param name="valor">montante a ser parcelado</param>
+        /// <param name="nparc">o número de parcelas (de acordo a RN25)</param>
+        /// <returns>o valor de cada parcela (de acordo com a RN26)</returns>
+        public double Calcular(double valor, int nparc) 
+        {
+            return valor / nparc;
+        }
+
+    }
+}
