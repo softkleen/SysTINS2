e9f69a0f6404d434c8bf188d427d75a59c240864
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2024-10-21 16:37:43 -0300

    inicio classe usuario

--- SysTINSClass/Banco.cs
+++ SysTINSClass/Banco.cs
@@ -10,7 +10,7 @@ namespace SysTINSClass
     // classe Banco é nossa classe de conexão
     public static class Banco // static pois não precisaremos criar uma instancia de Banco para conectar às nossas base dados
     {
-        public static MySqlCommand Abrir() // método para abrir conexão
+        public static MySqlCommand Abrir(int cod = 0) // método para abrir conexão
         {
             string strcon = @"server=127.0.0.1;database=systinsdb02;user=root;password=";
             MySqlConnection cn = new(strcon);
--- SysTINSClass/Usuario.cs
+++ SysTINSClass/Usuario.cs
@@ -1,12 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SysTINSClass
 {
-    internal class Usuario
+    public class Usuario
     {
+        public int Id;
+        public string? Nome;
+        public string? Email;
+        public string? Senha;
+        public Nivel Nivel;
+        public bool Ativo;
+
+        public Usuario() 
+        {
+            Nivel = new();
+        }
+        public Usuario(string nome, string email, string senha, Nivel nivel)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+        }
+        public Usuario(string nome, string email, string senha, Nivel nivel, bool ativo)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+        public Usuario(int id, string nome, string email, string senha, Nivel nivel, bool ativo)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+        // inserir 
+        public void Inserir() 
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_usuario_insert";
+            cmd.Parameters.Add("spnome",MySql.Data.MySqlClient.MySqlDbType.VarChar).Value=Nome;
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.Parameters.AddWithValue("spsenha", Senha);
+            cmd.Parameters.AddWithValue("spnivel", Nivel);
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Id = dr.GetInt32(0);
+            }
+            cmd.Connection.Close();
+        }
     }
 }
+
+//CREATE DEFINER =`root`@`localhost` PROCEDURE `sp_usuario_insert`(
+//-- parâmetros da procedure
+//spnome varchar(60), spemail varchar(60), spsenha varchar(32), spnivel int)
+//begin
+//	insert into usuarios 
+//	values (0, spnome, spemail, md5(spsenha), spnivel, default);
+//select* from usuarios where id = last_insert_id();
+//end$$
+
+
+//id int(4) AI PK 
+//nome varchar(60) 
+//email varchar(60) 
+//senha varchar(32) 
+//nivel_id int(11) 
+//ativo bit(1)
